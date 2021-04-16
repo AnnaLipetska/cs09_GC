05c8b6250f2cafb63bc2e045e66084f0f0054529
Anna <a.lipetska@gmail.com>
2021-04-16 20:49:16 +0300

    lesson 09 _ GC

---
+++ lesson09/task1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    // Создайте свой класс, объекты которого будут занимать много места в памяти (например, в
+    // коде класса будет присутствовать большой массив) и реализуйте для этого класса,
+    // формализованный шаблон очистки
+    public class MyResourceWrapper : IDisposable
+    {
+        // Используется для выяснения того, вызывался ли уже метод Dispose()
+        private bool disposed = false;
+        int[] bigArray;
+
+        public MyResourceWrapper()
+        {
+            bigArray = new int[1000];
+            for (int i = 0; i < bigArray.Length; i++)
+            {
+                bigArray[i] = i;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            // Вызов вспомогательного метода.
+            // Значение true указывает на то, что очистка
+            // была инициирована пользователем объекта.
+            Cleanup(true);
+            // Подавление финализации.
+            GC.SuppressFinalize(this);
+        }
+
+        private void Cleanup(bool disposing)
+        {
+            // Проверка, выполнялась ли очистка,
+            if (!this.disposed)
+            {
+                // Если disposing равно true, должно осуществляться
+                // освобождение всех управляемых ресурсов,
+                if (disposing)
+                {
+                    // Здесь осуществляется освобождение управляемых ресурсов.
+                    bigArray = null;
+                    Console.WriteLine("Освобождаем управляемые ресурсы.");
+                }
+                // Очистка неуправляемых ресурсов.
+                Console.WriteLine("Закрываем соединение с базой.");
+            }
+            disposed = true;
+        }
+
+        ~MyResourceWrapper()
+        {
+            // Вызов вспомогательного метода.
+            // Значение false указывает на то, что
+            // очистка была инициирована сборщиком мусора.
+            Cleanup(false);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка работы конструкции using:");
+            using (var myResourceWrapper = new MyResourceWrapper())
+            {
+                Console.WriteLine("Используем экземпляр нашего класса");
+            }
+
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine("Проверка ручного вызова метода Dispose:");
+            var wrapper = new MyResourceWrapper();
+
+            wrapper.Dispose();
+            wrapper.Dispose();
+
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine("Проверка работы деструктора:");
+
+            var wrapper4Finalizing = new MyResourceWrapper();
+        }
+    }
+}
---
+++ lesson09/task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        // Создайте класс, который позволит выполнять мониторинг ресурсов, используемых программой.
+        // Используйте его в целях наблюдения за работой программы, а именно: пользователь может
+        // указать приемлемые уровни потребления ресурсов(памяти), а методы класса позволят выдать
+        // предупреждение, когда количество реально используемых ресурсов приблизиться к
+        // максимально допустимому уровню.
+
+        class Watcher
+        {
+            int checksCounter = 0;
+            readonly int acceptableMemorySize;
+
+            public Watcher(int memorySize)
+            {
+                acceptableMemorySize = memorySize;
+            }
+
+            public void CheckMemory(object o)
+            {
+                Console.WriteLine(++checksCounter);
+                if (GC.GetTotalMemory(false) > acceptableMemorySize)
+                {
+                    Console.WriteLine("Houston, we have a problem");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var watcher = new Watcher(40000000);
+            var timer = new Timer(watcher.CheckMemory, new object(), 0, 100);
+
+            ArrayWrapper[] arrayOfArrays = new ArrayWrapper[10000];
+
+            for (int i = 0; i < arrayOfArrays.Length; i++)
+            {
+                var arr = new ArrayWrapper();
+            }
+        }
+    }
+
+    class ArrayWrapper
+    {
+        int[] array = new int[1000000];
+        public ArrayWrapper()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+        }
+
+        public int this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+
+        public int Length 
+        {
+            get { return array.Length; }
+        }
+    }
+}
